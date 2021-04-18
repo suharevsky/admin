4ef3997e3cc66eb8d12ca64b9530e6fbcfba840e
Suharevsky <aliksui.ua@mail.ru>
2021-04-18 14:04:11 +0300

    create service js resolver for pages

---
+++ src/app/services/js/js-page.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {JsService} from './js.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class JsPageService {
+
+    constructor(
+        public jsService: JsService
+    ) {
+    }
+
+    public home() {
+        this.jsService.playBgVideo();
+        this.jsService.fadeInSections();
+        this.jsService.arrowScrollDown();
+    }
+}
